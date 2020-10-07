b9a93a08c3834ce29f389932d46ca92004866c86
ivanpc\Юлия <juliyaone123@gmail.com>
2020-10-07 20:10:49 +0300

    Сделала анимацию заголовков самостоятельно

---
+++ projects/webim/js/animates-titles.js
@@ -0,0 +1,30 @@
+let titles = document.querySelectorAll('.installation__header');
+
+titles.forEach((el, i) => {
+  if (!el.classList.contains('installation__header--hidden')) {
+    el.classList.add('installation__header--hidden');
+  }
+
+  if (el.classList.contains('installation__header--active')) {
+    el.classList.remove('installation__header--active');
+  }
+});
+
+
+function scrollHendler() {
+  titles.forEach((el, i) => {
+
+    if (pageYOffset > (el.offsetTop - window.innerHeight - el.clientHeight) && pageYOffset < (el.offsetTop + el.clientHeight)) {
+
+      titles[i].classList.add('installation__header--active');
+      titles[i].classList.remove('installation__header--hidden');
+    } else {
+      titles[i].classList.remove('installation__header--active');
+      titles[i].classList.add('installation__header--hidden');
+    }
+  });
+};
+
+scrollHendler();
+
+window.addEventListener('scroll', scrollHendler);
